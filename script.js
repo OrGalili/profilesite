81c6202e7bd22bfbebe320ccd0337f1bfbfe7fb9
Or Galili <orgalil21@gmail.com>
2021-10-26 13:32:42 +0300

    animating skills

--- script.js
+++ script.js
@@ -11,16 +11,34 @@ setTimeout(
         
     }, 1500);
     
-setTimeout(
-    function() 
-    {
-        $(document).ready(function() {
-            $( '.hide' ).removeClass( 'hide'); 
-        });
-
-        $(document).ready(function() {
-            $('#hi').remove();
-        });
-
+    setTimeout(
+        function() 
+        {
+            $(document).ready(function() {
+                $( '.hide' ).removeClass( 'hide'); 
+            });
+            
+            $(document).ready(function() {
+                $('#hi').remove();
+            });
+            
+            
+        }, 3000);
         
-    }, 3000);
+        
+        /*good explanation for on scroll animation:*/
+        /*https://coolcssanimation.com/how-to-trigger-a-css-animation-on-scroll/*/
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    document.querySelector("#wpfskill").classList.add("bar","wpf");
+                    document.querySelector("#csharpskill").classList.add("bar","csharp");
+                    document.querySelector("#jsskill").classList.add("bar","js");
+                    document.querySelector("#cssskill").classList.add("bar","css");
+                    document.querySelector("#sqlskill").classList.add("bar","sql");
+                    return;
+                }
+            });
+        });
+        
+        observer.observe(document.querySelector('#skills'));
